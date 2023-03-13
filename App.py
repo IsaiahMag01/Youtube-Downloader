31eab17fd2ccbdfc1452e3e3bf14d870254ec421
Isaiah Magana <maganaisaiah4@gmail.com>
2023-03-12 17:42:07 -0700

    Create initial window frame

---
+++ App.py
@@ -0,0 +1,14 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+#System Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+#our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+
+#run app
+app.mainloop()
