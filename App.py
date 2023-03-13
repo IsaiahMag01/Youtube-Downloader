7900b2960392c90495e651a92b0b9cca4c8a9d7d
Isaiah Magana <maganaisaiah4@gmail.com>
2023-03-12 17:54:09 -0700

    Create the link input section of the application

--- App.py
+++ App.py
@@ -9,6 +9,16 @@ customtkinter.set_default_color_theme("blue")
 #our app frame
 app = customtkinter.CTk()
 app.geometry("720x480")
+app.title("Youtube Downloader")
+
+#Adding UI elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10) #puts our UI Element on the frame
+
+#Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
 
 #run app
 app.mainloop()
