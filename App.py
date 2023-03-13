7cf8a913fa51edc1740f8d70c959dde18ea6dccb
Isaiah Magana <maganaisaiah4@gmail.com>
2023-03-12 18:01:13 -0700

    Create download button with basic outputs to command line

--- App.py
+++ App.py
@@ -2,6 +2,18 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = Youtube(ytLink)
+        video = ytObject.streams.get_highest_resolution()
+        video.download()
+    except:
+        print("Youtube link is invalid") #Sends to the consol
+    print("Download Complete!")
+
+
 #System Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -19,6 +31,9 @@ title.pack(padx=10, pady=10) #puts our UI Element on the frame
 url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
+
+#Download button
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
 
 #run app
 app.mainloop()
